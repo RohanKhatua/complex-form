662d6c45d332362a745329cd860452490afb1bbd
RohanKhatua <contact@rohankhatua.dev>
2024-06-26 23:29:05 +0530

    style: change font size

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Backtesting Form</h1>
+      <h1 className="text-3xl font-bold mb-4">Backtesting Form</h1>
       <BacktestingForm />
       <MonacoEditor></MonacoEditor>
     </div>
